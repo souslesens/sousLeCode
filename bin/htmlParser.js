8250c73c0a35800866b27fe9d409109508a84c30
Claude Fauconnet <claude.fauconnet@gmail.com>
2024-08-17 19:44:10 +0200

    enrich  htmlParser.js

--- bin/htmlParser.js
+++ bin/htmlParser.js
@@ -5,7 +5,36 @@ const acorn = require("acorn");
 
 
 var HtmlParser = {
-
+    iconsMap: {
+        "arrow-icon": "../../icons/KGATheme/caret-right-KGA.png",
+        "search-icon": "../../icons/KGATheme/SearchIcon-KGA.png",
+        "add-icon": "../../icons/CommonIcons/add-icon.png",
+        "moreOptions-icon": "../../icons/KGATheme/MoreOptionsIcon-KGA.png",
+        "lineage-logo": "../../icons/KGATheme/lineageLogo-KGA.png",
+        "KGquery-logo": "../../icons/KGATheme/Kgquery-KGA.png",
+        "KGcreator-logo": "../../icons/KGATheme/Kgcreator-KGA.png",
+        "SPARQL-logo": "../../icons/KGATheme/SPARQL-KGA.png",
+        "admin-logo": "../../icons/KGATheme/Admin-KGA.png",
+        "ConfigEditor-logo": "../../icons/KGATheme/ConfigEditor-KGA.png",
+        "GraphManagement-logo": "../../icons/KGATheme/GraphManagement-KGA.png",
+        "OntoCreator-logo": "../../icons/KGATheme/OntoCreator-KGA.png",
+        "TimeLine-logo": "../../icons/KGATheme/TimeLine-KGA.png",
+        "logoInstance-icon": "../../icons/KGATheme/KGALogo.png",
+        "slsvLogo-icon": "../../icons/CommonIcons/slsvLogo.png",
+        "saveIcon": "../../icons/KGATheme/SaveIcon-KGA.png",
+        "deleteIcon": "../../icons/KGATheme/DeleteIcon-KGA.png",
+        "filterIcon": "../../icons/CommonIcons/Filtre.png",
+        "allPropertyIcon": "../../icons/BluishVioletTheme/SelectionAllIcon-bluishViolet.png",
+        "currentPropertyIcon": "../../icons/BluishVioletTheme/SelectionIcon-bluishViolet.png",
+        "jqueryClose": "../../icons/KGATheme/CrossIcon-KGA.png",
+        "resetIcon": "../../icons/KGATheme/ResetIcon-KGA.png",
+        "previousIcon": "../../icons/KGATheme/PreviousIcon-KGA.png",
+        "isDarkTheme": false,
+        "whiteBoardIcon": "../../icons/CommonIcons/WhiteboardIcon.png",
+        "propertiesIcon": "../../icons/CommonIcons/propertiesIcon.png",
+        "containersIcon": "../../icons/CommonIcons/ContainerIcon.png",
+        "classesIcon": "../../icons/CommonIcons/classesIcon.png"
+    },
 
     parseHtmlDir: function (rootDir, callback) {
 
@@ -14,9 +43,9 @@ var HtmlParser = {
 
         var allButtons = []
         var allIcons = []
-        var allClassIcons=[]
-        var allSelects=[]
-        var allInputs=[]
+        var allClassIcons = []
+        var allSelects = []
+        var allInputs = []
         var actionsMap = {}
         var options = {}
         util2.getFilesInDirRecursively(rootDir, options, function (err, dirs) {
@@ -42,7 +71,7 @@ var HtmlParser = {
                         if (!filePath.endsWith("html")) {
                             return;
                         }
-                      //  console.log(filePath);
+                        //  console.log(filePath);
 
 
                         var html = "" + fs.readFileSync(filePath)
@@ -54,30 +83,37 @@ var HtmlParser = {
 
                         var x = root
 
+                        var module=dirName.replace(rootDir,"").replace(/\\/g,"/")
+                        if(module=="")
+                            module="root"
+
                         // bouttons avec  icons fixes
                         var imgElements = root.getElementsByTagName("IMG")
                         imgElements.forEach(function (element) {
 
                             var srcValue = element.getAttribute("src")
-                         //   console.log(srcValue)
+                            //   console.log(srcValue)
                             if (srcValue && srcValue.indexOf("icon") > -1) {
                                 var buttonElement = element.parentNode
-
 
                                 var attributes = buttonElement.attributes
 
 
                                 for (var key in attributes) {
-                                    if (key.indexOf("on") == 0) {
-                                        allIcons.push({
-                                            file: file.name,
-                                            dir: dirName,
-                                            icon: srcValue,
-                                            innerHTML: buttonElement.innerHTML.replace(/[\r\t\n]/g, ""),
-                                            action: attributes[key]
-
-                                        })
+                                    if (!actionsMap[file.name + "_" + attributes[key]]) {
                                         actionsMap[file.name + "_" + attributes[key]] = 1
+                                        if (key.indexOf("on") == 0) {
+                                            allButtons.push({
+                                                file: file.name,
+                                                dir: module,
+                                                type: "commonIconButton",
+                                                icon: srcValue,
+                                                innerHTML: buttonElement.innerHTML.replace(/[\r\t\n]/g, ""),
+                                                action: attributes[key]
+
+                                            })
+
+                                        }
                                         //  console.log(element.innerHTML + "  " + key + "  " + attributes[key])
                                     }
 
@@ -91,7 +127,40 @@ var HtmlParser = {
                         var buttonElements = root.getElementsByTagName("BUTTON")
 
 
-                        // bouttons sans icons
+                        // bouttons sans icons fixes correspondant à une classe css
+                        buttonElements.forEach(function (buttonElement) {
+
+
+                            var attributes = buttonElement.attributes
+
+                            if (!actionsMap[file.name + "_" + attributes[key]]) {
+                                actionsMap[file.name + "_" + attributes[key]] = 1
+                                for (var key in attributes) {
+                                    if (key.indexOf("class") == 0) {
+                                        var classes = attributes[key]
+                                        for (var cssClass in HtmlParser.iconsMap) {
+                                            if (classes.indexOf(cssClass) > -1) {
+                                                allButtons.push({
+                                                    file: file.name,
+                                                    dir: module,
+                                                    type: "iconButton",
+                                                    icon: cssClass,
+                                                    innerHTML: buttonElement.innerHTML.replace(/[\r\t\n]/g, ""),
+                                                    action: attributes["onclick"] || attributes["onchange"]
+
+                                                })
+
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        })
+
+
+                        //bouttons standard sans icons
+                        var buttonElements = root.getElementsByTagName("BUTTON")
+
                         buttonElements.forEach(function (buttonElement) {
 
 
@@ -102,9 +171,11 @@ var HtmlParser = {
                                 if (key.indexOf("on") == 0) {
 
                                     if (!actionsMap[file.name + "_" + attributes[key]]) {
+                                        actionsMap[file.name + "_" + attributes[key]] = 1
                                         allButtons.push({
                                             file: file.name,
-                                            dir: dirName,
+                                            dir: module,
+                                            type: "simpleButton",
                                             icon: "none",
                                             innerHTML: buttonElement.innerHTML.replace(/[\r\t\n]/g, ""),
                                             action: attributes[key]
@@ -114,33 +185,6 @@ var HtmlParser = {
                                 }
                             }
                         })
-
-
-                        var buttonElements = root.getElementsByTagName("BUTTON")
-
-
-                        // bouttons sans icons fixes correspondant à une classe css
-                        buttonElements.forEach(function (buttonElement) {
-
-
-                            var attributes = buttonElement.attributes
-
-
-                            for (var key in attributes) {
-                                if (key.indexOf("class") == 0) {
-                                    allClassIcons.push({
-                                        file: file.name,
-                                        dir: dirName,
-                                        icon: attributes[key],
-                                        innerHTML: buttonElement.innerHTML.replace(/[\r\t\n]/g, ""),
-                                        action: attributes["onclick"] || attributes["onchange"]
-
-                                    })
-
-                                }
-                            }
-                        })
-
 
 
                         var buttonElements = root.getElementsByTagName("SELECT")
@@ -154,23 +198,24 @@ var HtmlParser = {
 
 
                             for (var key in attributes) {
-                                var action=""
-                                var actionType=""
+                                var action = ""
+                                var actionType = ""
                                 if (key.indexOf("on") == 0) {
-                                    action=attributes[key]
-                                    actionType=key
+                                    action = attributes[key]
+                                    actionType = key
                                 }
 
 
-                                        allSelects.push({
-                                            file: file.name,
-                                            dir: dirName,
-                                            icon: attributes["id"] || "",
-                                            innerHTML: buttonElement.innerHTML.replace(/[\r\t\n]/g, ""),
-                                            action: action,
-                                            actionType:actionType
+                                allSelects.push({
+                                    file: file.name,
+                                    dir: module,
+                                    type: "select",
+                                    icon: attributes["id"] || "",
+                                    innerHTML: buttonElement.innerHTML.replace(/[\r\t\n]/g, ""),
+                                    action: action,
+                                    actionType: actionType
 
-                                        })
+                                })
 
                             }
                         })
@@ -186,26 +231,26 @@ var HtmlParser = {
 
 
                             for (var key in attributes) {
-                                var action=""
-                                var actionType=""
+                                var action = ""
+                                var actionType = ""
                                 if (key.indexOf("on") == 0) {
-                                    action=attributes[key]
-                                    actionType=key
+                                    action = attributes[key]
+                                    actionType = key
                                 }
 
-                                    allInputs.push({
-                                        file: file.name,
-                                        dir: dirName,
-                                        icon: attributes["id"] || "",
-                                        innerHTML: buttonElement.innerHTML.replace(/[\r\t\n]/g, ""),
-                                        action: action,
-                                        actionType:actionType
+                                allInputs.push({
+                                    file: file.name,
+                                    dir: module,
+                                    type: "input",
+                                    icon: attributes["id"] || "",
+                                    innerHTML: buttonElement.innerHTML.replace(/[\r\t\n]/g, ""),
+                                    action: action,
+                                    actionType: actionType
 
-                                    })
+                                })
 
                             }
                         })
-
 
 
                     })
@@ -216,103 +261,30 @@ var HtmlParser = {
         })
 
 
+        HtmlParser.writeArray(allButtons, "slsbuttons")
 
-
-HtmlParser.writeArray(allButtons,"slsbuttons")
-
-        HtmlParser.writeArray(allIcons,"slsIcons")
-        HtmlParser.writeArray(allClassIcons,"slsClassIcons")
-        HtmlParser.writeArray(allSelects,"slsSelects")
-        HtmlParser.writeArray(allInputs,"slsInputs")
-
-
-
+        HtmlParser.writeArray(allSelects, "slsSelects")
+        HtmlParser.writeArray(allInputs, "slsInputs")
 
 
     },
-    writeArray:function(array,fileName){
-        var str="file\tdir\ticon\taction\tinnerHtml\tactionType\n"
-        array.forEach(function(item){
-            str+=item.file+"\t"+item.dir+"\t"+item.icon+"\t"+item.action+"\t"+item.innerHTML+"\r"
+    writeArray: function (array, fileName) {
+        var str = "file;dir;type;icon;action;innerHtml;actionType\n"
+        array.forEach(function (item) {
+            str += item.file + ";" + item.dir + ";" + item.type + ";" + item.icon + ";" + item.action + ";" + item.innerHTML + "\r"
         })
-        fs.writeFileSync("C:\\Users\\claud\\Downloads\\"+fileName+".csv",str)
-        var x=str
+        fs.writeFileSync("C:\\Users\\claud\\Downloads\\" + fileName + ".csv", str)
+        var x = str
 
     }
-    , htmlMapToCsv: function (htmlMap) {
-        var strIds = "DOMids\tpage\tlevel2\tlevel3\tlevel4\n"
-        var strFns = "label\tfunction\tpage\tfeature\n"
-
-        for (var html in htmlMap) {
-            var htmlObj = htmlMap[html]
-
-            var page = html.replace("D:\\webstorm\\souslesensVocables\\public\\vocables\\snippets\\", "")
-            var pageArray = page.split(path.sep)
-            /*   pageArray = pageArray.reverse()
-               var pageStr = ""
-               for (var i = 0; i < 4; i++) {
-                   var strx = ""
-                   if (pageArray.length > i)
-                       strx = pageArray[i];
-                   else
-                       strx = "level" + (i + 1);
-                   if (i > 0)
-                       pageStr += "\t"
-                   pageStr += strx
-               }*/
-            var pageStr = pageArray[1] + "\t" + pageArray[0]
 
 
-            var functions = ""
-            if (htmlObj.actions) {
-                htmlObj.actions.forEach(function (action, index) {
-                    if (action.function.indexOf("$") < 0) {
-                        strFns += action.label + "\t" + action.function + "\t" + pageStr + "\n"
-                    }
-                })
-            }
-            var DOMids = ""
-            if (htmlObj.DOMids) {
-
-                htmlObj.DOMids.forEach(function (id, index) {
-                    strIds += id + "\t" + pageStr + "\n"
-                })
-
-            }
 
 
-        }
-        strIds = strIds.replace(/undefined/g, "")
-        strFns = strFns.replace(/undefined/g, "")
-
-        fs.writeFileSync("D:\\sousLeSens\\code\\htmlIds.txt", strIds)
-        fs.writeFileSync("D:\\sousLeSens\\code\\actions.txt", strFns)
 
 
-    }
-    , extractEvents: function (filePath) {
-        var str = "" + fs.readFileSync(filePath)
-        var regex = /on(click|change)=["']([^"]+)"/gmi
-        var str2 = "" + str
-        var array = []
-        var map = {}
-        map = {}
-        while ((array = regex.exec(str)) != null) {
-            var id = HtmlParser.getRandomHexaId(5)
-            str2 = str2.replace(array[0], "data-eventId='" + id + "'")
-            map[id] = {file: filePath, event: array[1], fn: array[2]}
-        }
 
 
-        var x = str2;
-        var y = map
-
-
-    },
-    getRandomHexaId: function (length) {
-        const str = Math.floor(Math.random() * Math.pow(16, length)).toString(16);
-        return "0".repeat(length - str.length) + str;
-    }
 }
 module.exports = HtmlParser
 
